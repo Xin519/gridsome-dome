84b294c667974141f6e6d0e5a00de5e1c3e26077
falls <76679137+Xin519@users.noreply.github.com>
2021-03-25 12:10:19 +0800

    Update post.vue

--- src/pages/post.vue
+++ src/pages/post.vue
@@ -6,31 +6,12 @@
 				<div class="journal-header" data-v-2a0eef53="">
 					<h1 class="journal-title" data-v-2a0eef53="">Gridsome with Forestry CMS</h1>
 					<div class="journal-meta" data-v-2a0eef53="">
-						<div class="journal-author" data-v-2a0eef53="">
-							<span class="label" data-v-2a0eef53="">Author</span>
-							<span class="author-name" >Nichlas W. Andersen</span>
-						</div>
-						<div class="journal-date" data-v-2a0eef53="">
-							<span class="label" data-v-2a0eef53="">Date</span>
-							<div data-v-2a0eef53="">7. June 2019</div>
-						</div>
-						<div class="journal-time" data-v-2a0eef53="">
-							<span class="label" data-v-2a0eef53="">Time</span>
-							<span>1 min read</span>
-						</div>
+						
 					</div>
 				</div>
 				<div class="journal-content" data-v-2a0eef53="">
 					
-					<p>
-                        The&nbsp;<strong>banana</strong>&nbsp;fruits grow from a&nbsp;<strong>banana</strong>&nbsp;blossom in hanging
-                        clusters, also called a bunch or&nbsp;<strong>banana</strong> stem. The fruits grow in rows called tiers or hands.
-                        There can be as many as twenty fruits to a hand, and as many as twenty tiers in a bunch. A bunch usually weighs
-                        between 30 and 50 kilograms (65 to 110 pounds).
-                    </p>
-                    <p>
-                        <img src="/img/a1.jpg" class="g-image g-image--lazy g-image--loaded" sizes="(max-width: 2560px) 100vw, 2560px" width="2560">
-                    </p>
+				
 					
 				</div>
 			</div>
