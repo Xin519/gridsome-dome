b0207a6a94b0d567d449fafa2fb13811da1cfb65
falls <76679137+Xin519@users.noreply.github.com>
2021-03-25 12:09:23 +0800

    Update Post.vue

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -21,16 +21,6 @@
 					</div>
 				</div>
 				<div class="journal-content" data-v-2a0eef53="" style="margin-top:30px" v-html="mdToHtml($page.post.content)">
-					<!-- {{ $page.post.content }} -->
-					<!-- <p>
-                        The&nbsp;<strong>banana</strong>&nbsp;fruits grow from a&nbsp;<strong>banana</strong>&nbsp;blossom in hanging
-                        clusters, also called a bunch or&nbsp;<strong>banana</strong> stem. The fruits grow in rows called tiers or hands.
-                        There can be as many as twenty fruits to a hand, and as many as twenty tiers in a bunch. A bunch usually weighs
-                        between 30 and 50 kilograms (65 to 110 pounds).
-                    </p>
-                    <p>
-                        <img src="/img/a1.jpg" class="g-image g-image--lazy g-image--loaded" sizes="(max-width: 2560px) 100vw, 2560px" width="2560">
-                    </p> -->
 					
 				</div>
 			</div>
